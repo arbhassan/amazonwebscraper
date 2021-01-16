75c855cf8d79029b9cf56d9b34923f1bc4fea660
arbhassan <hassanjawad1321@gmail.com>
2021-01-16 20:41:51 +0500

    Getting imgURL, title and price from each product

---
+++ scrapers.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppetter");
+
+async function scrapeProduct(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const [el] = await page.$x('//*[@id="imgBlkFront"]');
+  const src = await el.getProperty("src");
+  const imgURL = await src.jsonValue();
+
+  const [el2] = await page.$x('//*[@id="productTitle"]');
+  const txt = await el2.getProperty("textContent");
+  const title = await txt.jsonValue();
+
+  const [el3] = await page.$x(
+    '//*[@id="buyNewSection"]/a/h5/div/div[2]/div/span[2]'
+  );
+  const txt2 = await el3.getProperty("textContent");
+  const price = await txt.jsonValue();
+
+  console.log({ imgURL, title, price });
+
+  broswer.close();
+}
+
+scrapeProduct(
+  "https://www.amazon.com/Black-Swan-Improbable-Robustness-Fragility/dp/081297381X"
+);
